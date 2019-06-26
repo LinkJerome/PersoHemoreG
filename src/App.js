3b357534b003f3707e8755a4ea9b747cc5ee6987
GIL JEROME <jerome.gil@etu.univ-lyon1.fr>
2019-06-26 19:22:41 +0700

    FIX: removed webpack

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { hot } from 'react-hot-loader/root';
 import React from 'react';
 import './App.css';
-import Website from './Website';
+import Website from './Website'; 
 
 function App() {
   return (
