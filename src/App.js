e309ea3f0344abe03f3ddaca01f42e236d3734ab
GIL JEROME <jerome.gil@etu.univ-lyon1.fr>
2019-06-27 21:18:40 +0700

    Best folders tree

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
 import { hot } from 'react-hot-loader/root';
 import React from 'react';
 import './App.css';
